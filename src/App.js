8ca3736ab1b03263cb50a271434093ee8ebba2e6
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-09-13 12:04:03 +0530

    Adding context for managing authentication state done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as  Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/todos' element={<TodoList/>}/>
       </Routes>
     </Router>
     </div>
---
+++ src/context/AuthContext.js
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+  };
+
+  export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [successMessage, setSuccessMessage] = useState('');
+    const navigate = useNavigate();
+  
+    const register = (name, email, password) => {
+      setUser({ name, email });
+      setSuccessMessage('Registration successful!');
+      setTimeout(() => {
+        navigate('/todos');
+      }, 2000);
+    };
+  
+    const login = (email, password) => {
+      setUser({ email });
+      navigate('/todos');
+    };
+  
+    const logout = () => {
+      setUser(null);
+      navigate('/login');
+    };
+  
+    return (
+      <AuthContext.Provider value={{ user, register, login, logout, successMessage }}>
+        {children}
+      </AuthContext.Provider>
+    );
+  };
