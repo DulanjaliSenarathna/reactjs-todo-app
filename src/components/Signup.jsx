496b44fcbc85f75ce7a79d8b99dbd58aafde77af
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-09-13 10:17:53 +0530

    Design JSX structure for signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,7 +3,24 @@ import './Signup.css'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='auth-form'>
+        <h2>Register</h2>
+        <form>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input id='name' name='name' type="text" />
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input id='email' name='email' type="email" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input id='password' name='password' type="password" />
+            </div>
+            <button type="submit">Register</button>
+        </form>
+    </div>
   )
 }
 
