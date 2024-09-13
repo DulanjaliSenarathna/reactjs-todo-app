feba58d9cdd76b92fa65c7e06a39a7d25b540733
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-09-13 14:52:09 +0530

    Created basic jsx structure of Todo management page and add success message

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import './Signup.css'
 import { Link } from 'react-router-dom'
 import { useFormik } from 'formik';
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TodoList.css'
+import { useAuth } from '../context/AuthContext'
 
 const TodoList = () => {
+    const {successMessage} = useAuth();
+
+    const [editingIndex, setEditingIndex] = useState(null);
   return (
-    <div>TodoList</div>
+    <div>
+        {successMessage && <p className="success-message">{successMessage}</p>}
+        
+        <div>
+            <h2>{editingIndex !== null ? 'Edit Todo' : 'Add Todo'}</h2>
+            <input type="text" placeholder='Title' />
+            <textarea placeholder='Description'></textarea>
+            <button>Add</button>
+        </div>
+
+        <ul>
+            <li>Sample todo</li>
+        </ul>
+    </div>
   )
 }
 
