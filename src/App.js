059550c577efce56d6e834ed3b8e99a2d852400f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-09-13 12:50:37 +0530

    Signup form validation and done with navigate to todolist page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import './App.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import TodoList from './components/TodoList';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <div>
     <Router>
+    <AuthProvider>
+    <div className="app">
       <Routes>
         <Route path='/' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/todos' element={<TodoList/>}/>
       </Routes>
+      </div>
+    </AuthProvider>
     </Router>
-    </div>
-   
   );
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,23 +1,57 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Signup.css'
 import { Link } from 'react-router-dom'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useAuth } from '../context/AuthContext';
 
 const Signup = () => {
+
+    const {register} = useAuth();
+
+    const formik = useFormik({
+        initialValues: {
+          name: '',
+          email: '',
+          password: '',
+        },
+        validationSchema: Yup.object({
+          name: Yup.string().required('Required'),
+          email: Yup.string().email('Invalid email').required('Required'),
+          password: Yup.string().min(6, 'Min 6 characters').required('Required'),
+        }),
+        onSubmit: (values) => {
+          register(values.name, values.email, values.password);
+        },
+      });
+
   return (
     <div className='auth-form'>
         <h2>Register</h2>
-        <form>
+        <form onSubmit={formik.handleSubmit}>
             <div>
                 <label htmlFor="name">Name</label>
-                <input id='name' name='name' type="text" />
+                <input id='name' name='name' type="text" onChange={formik.handleChange}
+                value={formik.values.name}/>
+                {formik.touched.name && formik.errors.name ? (
+                <div className="error">{formik.errors.name}</div>
+                ) : null}
             </div>
             <div>
                 <label htmlFor="email">Email</label>
-                <input id='email' name='email' type="email" />
+                <input id='email' name='email' type="email" onChange={formik.handleChange}
+                value={formik.values.email} />
+                 {formik.touched.email && formik.errors.email ? (
+                <div className="error">{formik.errors.email}</div>
+                ) : null}
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input id='password' name='password' type="password" />
+                <input id='password' name='password' type="password" onChange={formik.handleChange}
+                value={formik.values.password} />
+                {formik.touched.password && formik.errors.password ? (
+                <div className="error">{formik.errors.password}</div>
+                ) : null}
             </div>
             <button type="submit">Register</button>
             <p>Already have an account? <Link to='/login'>Login here</Link> </p>
